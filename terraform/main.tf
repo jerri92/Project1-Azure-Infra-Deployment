96103a2c46a85425443002c972b03b2e4c4287a6
Jerin Joshy <jerrijacobjoshy@gmail.com>
2025-08-01 16:48:45 +1200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "webapp-rg"
-  location = "australia-east"
+  name     = "iac-demo-rg"
+  location = var.location
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name                = "webapp-plan"
+  name                = "iac-demo-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
   sku {
     tier = "Basic"
     size = "B1"
@@ -18,7 +19,7 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "my-webapp-demo"
+  name                = "iac-demo-webapp-${random_string.suffix.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
@@ -30,4 +31,12 @@ resource "azurerm_app_service" "app" {
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  lower   = true
+  number  = true
+  special = false
 }
