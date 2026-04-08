51c3745202a87b794832e8eca045edebbdceff7e
Jerin Joshy <jerrijacobjoshy@gmail.com>
2026-04-08 15:28:10 +1200

    Update Azure backend configuration for Terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageacct"
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstatejerri"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "hub-spoke-dev.tfstate"
   }
 }
